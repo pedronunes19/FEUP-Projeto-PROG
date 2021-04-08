35a5401a7b9bdf473cf620442dfda079b40d36ef
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-04-08 17:10:05 +0100

    commit

--- robotgame.cpp
+++ robotgame.cpp
@@ -3,6 +3,7 @@
 #include <chrono>  // can handle with time
 #include <thread>
 #include <fstream>  // used for file handling
+#include <vector>
 using namespace std;
 
 // declaring functions
@@ -11,7 +12,7 @@ void rules(bool &programExecuting);
 void play(bool &programExecuting);
 bool fileExists(string fileName);
 void getMapSize(string firstLine, int &height, int &widht);
-void getMapArray(ifstream mapFile, string currentLine);
+void getMap(ifstream &mapFile, vector <vector <char>> &map);
 
 int main() {
 
@@ -42,7 +43,6 @@ int main() {
                 break;
             case 2:                                                     // Play (starts game)
                 play(programExecuting);
-                this_thread::sleep_for(chrono::seconds(TIMESLEEP));
                 break;
             default:                                                    // in case no valid option is selected
                 cout << "\nPlease choose a valid option" << endl;
@@ -72,7 +72,9 @@ void menu(){  // function to draw menu
 
 void rules(bool &programExecuting){  // function to display rules
     char exitRules;
-    cout << "\nThis will be the Rules page (press any character to return to the menu) ";
+    cout << "\nRULES\n\nYou can exit the game at any time with CTRL-Z (Windows) or CTRL-D (Linux)"
+         << "\n..."
+         << "\nPress any character when you're ready to leave";
     cin >> exitRules;  // wait for user input to return to menu
     if (cin.eof())     // more CTRL-Z CTRL-D stuff
         programExecuting = false;
@@ -83,38 +85,57 @@ void play(bool &programExecuting){  // function to play the game
     string mapNumber, mapFilePath, firstLine, currentLine;
     bool noFile = true;  // loop variable for file check
     int mapHeight, mapWidth;  // map dimensions
-    while(noFile){
+
+    // run loop until a existing file is found
+    while(noFile){ 
         cin.ignore(1);  // cleans input allowing getline() to wait for input
         cout << "\nSelect which map you would like to play (ex. 01, 02, ..., 99): " << endl;    
         getline(cin, mapNumber);
+        if (cin.eof()){
+            programExecuting = false;
+            return;
+        }
         mapFilePath = "MAZE_" + mapNumber + ".txt";  // creates file name from number chosen by user
         if (fileExists(mapFilePath))
             break;
         cout << "File doesn't exist";
     }
     
-    ifstream mapFile(mapFilePath); // opens map file for reading
+    ifstream mapFile(mapFilePath);  // opens map file for reading
     
     getline(mapFile, firstLine);
     getMapSize(firstLine, mapHeight, mapWidth);
-
-    string map[mapHeight][mapWidth]; // creates map array
+    vector <vector <char>> map;
+    getMap(mapFile, map); 
+    
+    for (int i = 0; i < map.size(); i++){
+        for (int j = 0; map[i].size(); j++){
+            cout << map[i][j];
+        }
+        cout << endl;
+    }
+    
 }
 
-bool fileExists(string fileName){
+bool fileExists(string fileName){  // function to check if a map file exists
     ifstream file(fileName);
     return file.good();
 }
 
-void getMapSize(string firstLine, int &height, int &width){  // 
+void getMapSize(string firstLine, int &height, int &width){  // function to get map size (height, width)
     const int pos = firstLine.find( " " );
     height = stoi(firstLine.substr( 0, pos ));
     width = stoi(firstLine.substr( pos + 3, string::npos ));
 }
 
-void getMapArray(ifstream mapFile, string currentLine = ""){
+void getMap(ifstream &mapFile, vector <vector <char>> &map){
+    int i = 0;
+    string currentLine = "";
     while (getline(mapFile,currentLine)){
-
+        for (int j = 0; j < currentLine.length(); j++) {
+            map.at(i).push_back(currentLine[j]);
+        }
+        i++;
     }
     
 }
